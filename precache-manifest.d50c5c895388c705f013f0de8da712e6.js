91fdf5a602521b18b5c6a2172e2402b8ca17076c
brave-aditya <pratapaditya84@gmail.com>
2020-08-02 00:19:48 +0530

    Updates

---
+++ precache-manifest.d50c5c895388c705f013f0de8da712e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc8f0aab184d8e86db663954505dfe4",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "ba71de4a86abbc019917",
+    "url": "/snake/static/css/main.68ebc3c9.chunk.css"
+  },
+  {
+    "revision": "ef65fe84b449753d04d8",
+    "url": "/snake/static/js/2.a9011972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.a9011972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba71de4a86abbc019917",
+    "url": "/snake/static/js/main.814523cc.chunk.js"
+  },
+  {
+    "revision": "330302f1424891b30bb2",
+    "url": "/snake/static/js/runtime-main.f5fb9ecf.js"
+  }
+]);
